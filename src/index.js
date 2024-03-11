81b8fd6bd5e7fa38d2aca78aeb73e698b679d580
app1 <saieesh.soma1919@gmail.com>
2024-03-11 16:31:21 +0530

    Last Commit

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import App14 from './App14';
 import App from './App';
 import App15 from './App15';
 import App16 from './App16';
+import App13a from './App13a';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App16 />
+<App13a />
 );

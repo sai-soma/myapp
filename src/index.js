93aba9a40966642429d70fcf65884e70fab48005
app1 <saieesh.soma1919@gmail.com>
2024-03-12 16:28:05 +0530

    env variable

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App13a from './App13a';
 import App15 from './App15';
+import App20 from './App20';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App15 />
+<App20 />
 );
